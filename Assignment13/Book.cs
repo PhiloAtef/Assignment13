6c163c81e33541afd452d8301cc44e780b611736
Philo <philoatefdev@gmail.com>
2024-08-01 22:22:18 +0300

    part 1

---
+++ Assignment13/Book.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment13
+{
+    public delegate string BookDelegate(Book book);
+
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string iSBN, string title, string[] authors, DateTime publicationDate, decimal price)
+        {
+            ISBN = iSBN;
+            Title = title;
+            Authors = authors;
+            PublicationDate = publicationDate;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"ISBN: {ISBN}, Title: {Title}, Authors: {Authors}, PublicationDate: {PublicationDate}, Price: {Price}";
+        }
+    }
+
+    public class BookFunctions
+    {
+        public static string GetTitle(Book book)
+        {
+            return book.Title;
+        }
+
+        public static string[] GetAuthors(Book book)
+        {
+            return book.Authors;
+        }
+
+        public static decimal GetPrice(Book book)
+        {
+            return book.Price;
+        }
+
+        public string GetISBN(Book book)
+        {
+            return book.ISBN;
+        }
+
+        public DateTime GetPublicationDate(Book book)
+        {
+            return book.PublicationDate;
+        }
+    }
+
+    public class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> booksList, BookDelegate bookDelegate)
+        {
+            foreach (Book book in booksList)
+            {
+                Console.WriteLine(bookDelegate(book));    
+            }
+        }
+
+        public void ProcessBooks<T>(List<Book> books, Func<Book, T> fPtr)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine(fPtr(book));
+            }
+        }
+        
+    }
+}
--- Assignment13/Program.cs
+++ Assignment13/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Assignment13
+﻿using System.Diagnostics;
+
+namespace Assignment13
 {
     internal class Program
     {
+        public static void ProcessBooks<T>(List<Book> books, Func<Book, T> fPtr)
+        {
+            foreach (Book book in books)
+            {
+                Console.WriteLine(fPtr(book));
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region
+            Book book1 = new("isbn1", "title1", ["author1","author2"],DateTime.Now,30);
+            Book book2 = new("isbn2", "title2", ["author2", "author3"], DateTime.Now, 30);
+
+            var books = new List<Book> { book1, book2 };
+            var bookFunctions = new BookFunctions();
+            var bookProcessor = new LibraryEngine();
+
+            ProcessBooks(books, BookFunctions.GetTitle);
+            ProcessBooks(books, BookFunctions.GetAuthors);
+            ProcessBooks(books, BookFunctions.GetPrice);
+
+            //anonymous
+            Func<Book, string> GetISBN = delegate (Book book) { return book.ISBN; };
+            ProcessBooks(books, GetISBN);
+
+            //lambda
+            ProcessBooks(books, (b) => b.PublicationDate);
+            #endregion
+
         }
     }
 }
